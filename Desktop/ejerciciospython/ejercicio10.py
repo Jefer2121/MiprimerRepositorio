376ca07c2824916c4d05881e597d1c8ec1205d90
Jefer2121 <jefermejia8@gmail.com>
2026-03-20 13:27:07 -0600

    listo

---
+++ Desktop/ejerciciospython/ejercicio10.py
@@ -0,0 +1,9 @@
+## verificar si la frase termina con punto
+## usar método endswith()
+
+frase = "Mañana tengo partido."
+
+if frase.endswith("."):
+    print("La frase termina con un punto")
+else:
+    print("La frase no termina con un punto")
