65fe1fee214285c9b31c8c3774e253aa9efd1254
Jefer2121 <jefermejia8@gmail.com>
2026-04-09 20:01:27 -0600

    Parcial 02 Fundamentos de programacion ejercicio 2 este ejercicio utiliza title para convertir las iniciales del nombre y apellido a mayusculas y luego emplee replace para buscar el nombre especifico y sustituirlo por el apellido

---
+++ ParcialII/ejercicio2.py
@@ -0,0 +1,10 @@
+# 1. Toma la cadena de texto
+texto = "jefersson mejia"
+
+# 2. Convierte la primera letra de cada palabra a mayúscula
+texto_cap = texto.title()
+
+# 3. Reemplaza "Jefersson" por "Mejia"
+nuevo_texto = texto_cap.replace("Jefersson", "Mejia")
+
+print(f"Resultado final: {nuevo_texto}")
