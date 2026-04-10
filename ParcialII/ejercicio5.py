53d1c17a1cc331eeb333270a6c50c2af74dbc301
Jefer2121 <jefermejia8@gmail.com>
2026-04-09 20:26:08 -0600

    Parcial fundamentos de programacion ejercicio 5 utilice .swapcase() para invertir tipos de letra y .Ijust() para ajustar el texto a la izquierda rellenando con asteriscos exactos

---
+++ ParcialII/ejercicio5.py
@@ -0,0 +1,10 @@
+# 1. Cadena inicial
+cadena5 = "pYTHON"
+
+# 2. Invertir mayúsculas/minúsculas
+invertido = cadena5.swapcase()
+
+# 3. Alineación a la izquierda con 15 espacios y relleno "*"
+resultado5 = invertido.ljust(15, "*")
+
+print(resultado5)
