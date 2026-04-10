06504ff3919c46bc5a35aa28aaddb8160bb876c3
Jefer2121 <jefermejia8@gmail.com>
2026-04-09 20:49:16 -0600

    Parcial 02 Fundamentos de programacion ejercicio 9 usamos el metodo el .replace() para una situacion exacta respetando espacios y .upper() para convertir toda la frase resultante a letras mayusculas

---
+++ ParcialII/ejercicio9.py
@@ -0,0 +1,11 @@
+# 1. Cadena inicial
+cadena9 = "Any time y Anytime"
+
+# 2. Reemplazar "Any time " (con espacio) por "Always"
+# Ojo: el espacio es clave para la rúbrica
+reemplazo = cadena9.replace("Any time ", "Always ")
+
+# 3. Todo a mayúsculas
+resultado9 = reemplazo.upper()
+
+print(f"Resultado: {resultado9}")
